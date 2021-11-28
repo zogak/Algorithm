a39408117256b7907e664548b565245dfc704efa
zogak <zogak7@gmail.com>
2021-11-28 20:36:58 +0900

    feat : Add leetCode num200

--- leetcode/200.py
+++ leetcode/200.py
@@ -1,3 +1,30 @@
 '''
 200. Number of Islands
 '''
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        def dfs(x, y):
+            if x < 0 or y < 0 or x >=n or y >=m:
+                return
+            if grid[x][y] == '1':
+                grid[x][y] = '0'
+
+                dfs(x-1, y)
+                dfs(x, y+1)
+                dfs(x+1, y)
+                dfs(x, y-1)
+            return
+    
+        n = len(grid)
+        m = len(grid[0])
+        
+        count = 0
+        
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == '1':
+                    dfs(i, j)
+                    count += 1
+        return count
+        
