e5ea34f35c75f4a5f07c954d6036e68ab15cebe4
zogak <zogak7@gmail.com>
2022-01-15 01:33:20 +0900

    fix : swea5176

--- swea/5176.py
+++ swea/5176.py
@@ -5,13 +5,13 @@
 import sys
 sys.stdin = open("input.txt", "r")
 
-def preorder(idx):
+def inorder(idx):
     global value
     if idx <= n:
-        preorder(idx*2)
+        inorder(idx*2)
         tree[idx] = value
         value += 1
-        preorder(idx*2 + 1)
+        inorder(idx*2 + 1)
 
 T = int(input())
 for test_case in range(1, T + 1):
@@ -19,5 +19,12 @@ for test_case in range(1, T + 1):
     tree = [0 for _ in range(n+1)]
 
     value = 1
-    preorder(1)
+    inorder(1)
     print("#{} {} {}".format(test_case, tree[1], tree[n//2]))
+
+'''
+3
+6
+8
+15
+'''
