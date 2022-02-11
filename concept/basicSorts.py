87abdfe075803ac92c27f56d29dabd0ea5261a0b
zogak <zogak7@gmail.com>
2022-02-11 20:50:19 +0900

    feat : Add basic sort

---
+++ concept/basicSorts.py
@@ -0,0 +1,25 @@
+#bubble sort
+def bubble_sort(my_list):
+    for i in range(len(my_list)-1, 0, -1):
+        for j in range(i):
+            if my_list[j] > my_list[j+1]:
+                my_list[j], my_list[j+1] = my_list[j+1], my_list[j]
+    return my_list
+
+print(bubble_sort([4, 2, 6, 5, 1, 3]))
+
+#selection sort
+def selection_sort(my_list):
+    for i in range(len(my_list)-1):
+        min_index = i
+        for j in range(i+1, len(my_list)):
+            if my_list[j] < my_list[min_index]:
+                min_index = j
+        if i != min_index:
+            temp = my_list[i]
+            my_list[i] = my_list[min_index]
+            my_list[min_index] = temp
+    return my_list
+
+print(selection_sort([4, 2, 6, 5, 1, 3]))
+
