736de70685dc78869e251f2b22ccbda0bef542c5
zogak <zogak7@gmail.com>
2022-03-20 00:54:51 +0900

    feat : insertion sort

--- concept/basicSorts.py
+++ concept/basicSorts.py
@@ -23,3 +23,12 @@ def selection_sort(my_list):
 
 print(selection_sort([4, 2, 6, 5, 1, 3]))
 
+#insertion sort
+def insertion_sort(my_list):
+    for i in range(1, len(my_list)):
+        temp = my_list[i]
+        j = i-1
+        while temp < my_list[j] and j > -1:
+            my_list[j+1] = my_list[j]
+            my_list[j] = temp
+            j -= 1
