291b36cf47c3fe64e9b20cf6ec692c83e02c244f
zogak <zogak7@gmail.com>
2022-09-25 14:09:48 +0900

    boj

---
+++ BOJ/15685.py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+dx = [0,-1,0,1]
+dy = [1,0,-1,0]
+graph = [[0]*101 for _ in range(101)]
+
+for _ in range(N):
+    y,x,d,g = map(int, input().split())
+    graph[x][y] = 1
+
+    curve = [d]
+    temp = [d]
+
+    for gen in range(g+1):
+        for t in temp:
+            x += dx[t]
+            y += dy[t]
+            graph[x][y] = 1
+        
+        temp = [(c+1)%4 for c in curve]
+        temp.reverse()
+        curve += temp
+
+cnt = 0
+for i in range(101):
+    for j in range(101):
+        if graph[i][j]==1 and graph[i+1][j]==1 and graph[i+1][j+1]==1 and graph[i][j+1]==1:
+            cnt += 1
+
+print(cnt)
