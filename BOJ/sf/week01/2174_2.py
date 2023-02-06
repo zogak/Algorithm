7123d8ec39479f88c23b41780b75c62a4ddb5e12
zogak <zogak7@gmail.com>
2023-02-07 00:46:38 +0900

    pass

--- BOJ/sf/week01/2174_2.py
+++ BOJ/sf/week01/2174_2.py
@@ -3,7 +3,6 @@ n, m = map(int, input().split())
 dx = [-1,0,1,0]
 dy = [0,1,0,-1]
 dirInfo = ['N','E','S','W']
-flag = -1
 check = [[0]*(a+1) for _ in range(b+1)]
 robots = [[]]
 for i in range(n):
@@ -22,15 +21,13 @@ for _ in range(m):
             curX = curX + dx[curDir]
             curY = curY + dy[curDir]
             
-            if curX<0 or curY<0 or curX>=b or curY>=a:
-                flag = 0
+            if curX<=0 or curY<=0 or curX>b or curY>a:
                 print('Robot {} crashes into the wall'.format(robotNum))
-                exit()
+                exit(0)
             
             if check[curX][curY] != 0:
-                flag = check[curX][curY]
-                print('Robot {} crashes into robot {}'.format(robotNum, flag))
-                exit()
+                print('Robot {} crashes into robot {}'.format(robotNum, check[curX][curY]))
+                exit(0)
 
     elif where == 'L':
         curDir = (curDir-howMany)%4
@@ -38,6 +35,8 @@ for _ in range(m):
     elif where == 'R':
         curDir = (curDir+howMany)%4
 
+    check[robots[robotNum][0]][robots[robotNum][1]] = 0
     robots[robotNum] = [curX, curY, curDir]
+    check[curX][curY] = robotNum
 
 print('OK')
